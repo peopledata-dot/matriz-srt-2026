820072a027047247c642c3b0f9e9b68880bb60c5
peopledata-dot <people.data@cangurovenezuela.com>
2026-03-12 13:42:09 -0400

    Updates

--- static/js/156.ea7d5798.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(globalThis.webpackChunkmatriz_srt_2026=globalThis.webpackChunkmatriz_srt_2026||[]).push([[156],{156(a,s,t){new Map,new Date}}]);
-//# sourceMappingURL=156.ea7d5798.chunk.js.map
---
+++ static/js/488.7e11beab.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(globalThis.webpackChunkmatriz_srt_2026=globalThis.webpackChunkmatriz_srt_2026||[]).push([[488],{488(a,s,t){new Map,new Date}}]);
+//# sourceMappingURL=488.7e11beab.chunk.js.map
